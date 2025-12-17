ba9a53516ba7409aea3bf12458343482e193cc46
James Bergstra <171276+jaberg@users.noreply.github.com>
2025-12-17 16:24:17 -0500

    stronger tests

--- test_200.py
+++ test_200.py
@@ -29,14 +29,15 @@ def test_home_about():
 
 @pytest.mark.parametrize("catpath", ipcc_canada.catpaths)
 def test_each_ipcc_sector(catpath):
+    assert len(ipcc_canada.catpaths) == 71 # what should this number be?
     url = f"/ipcc-sectors/{catpath}/"
     response = client.get(url)
     assert response.status_code == 200
 
 @pytest.mark.parametrize("idea_name", app.peval.projects)
 def test_each_strategy(idea_name):
+    assert len(app.peval.projects) > 3
     url = f"/strategies/{idea_name}/"
-    print(url)
     response = client.get(url)
     assert response.status_code == 200
 
