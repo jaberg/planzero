e4c2319557ce740cec8b179fd71656578095b209
James Bergstra <171276+jaberg@users.noreply.github.com>
2026-05-07 22:44:41 -0400

    adjust test_cattle numbers to new barrier behaviour

--- planzero/test_cattle.py
+++ planzero/test_cattle.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 from .cattle import *
 from .base import Scenario
+from .strategies.strategy2 import Scale_Bovaer
 
 
 def test_cattle_CH4_emissions_sum():
@@ -8,14 +9,17 @@ def test_cattle_CH4_emissions_sum():
     scenario.add_dynamic_elements([
         Cattle_Population_AR(),
         Cattle_Enteric_Emission_Rates_NIR2025_Bovaer(),
+        Bovaer_Adoption_Limit(),
+        Scale_Bovaer()
     ])
     scenario.run_until(2040 * u.years)
     emissions = scenario.compute_annual_emissions()
     n_provinces_and_territories_reporting = 10
     assert len(emissions.by_sector_ghg_pt_driver) == (
         n_provinces_and_territories_reporting * len(Livestock_nonsums))
-    print(emissions.by_sector_ghg_pt_driver.keys())
-    assert 1160142 < emissions.sum().magnitude < 1160150
+    #print(emissions.by_sector_ghg_pt_driver.keys())
+    val = emissions.sum().magnitude
+    assert 1129142 < val < 1140150
 
 
 def test_cattle_CO2_emissions_sum():
@@ -23,13 +27,16 @@ def test_cattle_CO2_emissions_sum():
     scenario.add_dynamic_elements([
         Cattle_Population_AR(),
         Bovaer_Production_Emission_Factors(),
+        Bovaer_Adoption_Limit(),
+        Scale_Bovaer()
     ])
     scenario.run_until(2040 * u.years)
     emissions = scenario.compute_annual_emissions()
     n_provinces_and_territories_reporting = 10
     assert len(emissions.by_sector_ghg_pt_driver) == (
         n_provinces_and_territories_reporting * len(Livestock_nonsums))
-    assert 23000 < emissions.sum().magnitude < 24000
+    val = emissions.sum().magnitude
+    assert 1500 < val < 1600
 
 
 def test_cattle_emissions_sum():
@@ -38,13 +45,16 @@ def test_cattle_emissions_sum():
         Cattle_Population_AR(),
         Bovaer_Production_Emission_Factors(),
         Cattle_Enteric_Emission_Rates_NIR2025_Bovaer(),
+        Bovaer_Adoption_Limit(),
+        Scale_Bovaer()
     ])
     scenario.run_until(2040 * u.years)
     emissions = scenario.compute_annual_emissions()
     n_provinces_and_territories_reporting = 10
     assert len(emissions.by_sector_ghg_pt_driver) == 2 * (
         n_provinces_and_territories_reporting * len(Livestock_nonsums))
-    assert 1183290 < emissions.sum().magnitude < 1183300.
+    val = emissions.sum().magnitude
+    assert 1130290 < val < 1143300.
 
 
 def test_cattle_bavaer_subsidy_sum():
@@ -75,4 +85,4 @@ def test_cattle_bavaer_subsidy_sum():
     cad = subsidies_strat.sum() - subsidies.sum()
     cost_per_tonne = (cad / co2e).to(u.CAD / u.tonne_CO2e)
     print(cost_per_tonne)
-    assert 210 < cost_per_tonne.magnitude < 212
+    assert 220 < cost_per_tonne.magnitude < 225
